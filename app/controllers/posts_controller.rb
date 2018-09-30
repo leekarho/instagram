1de5b18862a80d9e69819654164b5c0cb1e871fb
leekarho <k-h_lee@hotmail.co.uk>
2018-09-30 22:37:52 +0100

    need image to post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,7 +5,13 @@ class PostsController < ApplicationController
 
   def create
     @post = Post.create(post_params)
-    redirect_to @post
+    if @post.save
+      flash[:success] = "Your post has been created."
+      redirect_to @post
+    else
+      flash[:alert] = "You need an image to post here!"
+      render :new
+    end
   end
 
   def new
